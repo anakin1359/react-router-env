9421c996aa31aec03f6d301801ea8da1c344798e
anakin1359 <anakin1359@gmail.com>
2022-07-02 21:01:07 +0900

    add react-router-dom nest-pages

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
 export const Page1 = () => {
     return (
         <div>
             <h1>Page1 Page</h1>
+            <Link to="/page1/detailA">Link to DetailA</Link>
+            <br />
+
+            <Link to="/page1/detailB">Link to DetailB</Link>
+            <br />
         </div>
     )
 }

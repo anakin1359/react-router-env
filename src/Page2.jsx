a8895c28bc9620637eda24ce0da266e6583d3984
anakin1359 <anakin1359@gmail.com>
2022-07-03 00:00:20 +0900

    add Query Parameter bridge

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,6 +5,9 @@ export const Page2 = () => {
         <div>
             <h1>Page2 Page</h1>
             <Link to="/page2/1359">React URL Parameter</Link>
+            <br/>
+
+            <Link to="/page2/1359?name=query_parameter">Query Parameter</Link>
         </div>
     )
 }
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,12 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { ParameterId } = useParams();
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+
     console.log("ParameterId: ", ParameterId);
+    console.log("search     : ", search);
+    console.log("queryObject: ", query);
+    console.log("query's get: ", query.get);
+
     return (
         <div>
             <h1>Url Parameter Page</h1>
             <p>Parameter is {ParameterId}.</p>
+            <p>Query Parameter is {query.get("name")}.</p>
         </div>
     );
 };

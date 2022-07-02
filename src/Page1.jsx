8ab1a03778649553af3ac50921c4089cac744210
anakin1359 <anakin1359@gmail.com>
2022-07-03 00:33:42 +0900

    Screen transition processing with JavaScript

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 
 export const Page1 = () => {
     const sampleArray = [...Array(25).keys()];
     console.log("sampleArray: ", sampleArray);
+
+    // Screen transition processing with JavaScript
+    const pageHistory = useHistory();
+    const onClickDetailA = () => pageHistory.push("/page1/detailA");
+
     return (
         <div>
             <h1>Page1 Page</h1>
@@ -17,6 +22,12 @@ export const Page1 = () => {
 
             <Link to="/page1/detailB">Link to DetailB</Link>
             <br />
+
+            <button
+                onClick={onClickDetailA}
+            >
+                Link to DetailA JS
+            </button>
         </div>
     )
 }
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,12 +1,21 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export const Page1DetailA = () => {
     const { state } = useLocation();
     console.log("state: ", state);
 
+    // Screen transition processing with JavaScript
+    const pageBackHistory = useHistory();
+    const onClickBack = () => pageBackHistory.goBack();
+
     return (
         <div>
             <h1>Page1DetailA Page</h1>
+            <button
+                onClick={onClickBack}
+            >
+                previous
+            </button>
         </div>
     );
 };

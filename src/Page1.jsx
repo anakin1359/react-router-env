47bc4bf9f58de0874e0519975e541d5af3384dce
anakin1359 <anakin1359@gmail.com>
2022-07-03 00:19:06 +0900

    add Passing state between pages

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
 
 export const Page1 = () => {
+    const sampleArray = [...Array(25).keys()];
+    console.log("sampleArray: ", sampleArray);
     return (
         <div>
             <h1>Page1 Page</h1>
-            <Link to="/page1/detailA">Link to DetailA</Link>
+            {/* <Link to="/page1/detailA">Link to DetailA</Link> */}
+            <Link to={{
+                pathname: "/page1/detailA",
+                state: sampleArray
+            }}>
+                Link to DetailA
+            </Link>
             <br />
 
             <Link to="/page1/detailB">Link to DetailB</Link>
